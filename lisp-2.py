07e598b21b38c0df73a170b73cff8ea25d86e429
Maxwell Gisborne <105882380+maxwell-gisborne@users.noreply.github.com>
2023-11-06 18:12:52 +0000

    added note about lisp-2

--- lisp-2.py
+++ lisp-2.py
@@ -1,1 +1,9 @@
-RIP should have used version controll
+RIP should have used version control
+
+
+This was an implementation mush like lisp-1, but using iterators and termios
+to have a nicer cli experience.
+
+I carried some of this decisions and solutions forward into lisp-3
+but decided to use pythons readline library for the basic cli stuff, 
+and focus more on the lisp itself
